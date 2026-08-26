4a5db182f0cda99bca6a4d551fc709e08a0df539
MrCodeBS <singhbhupider206@gmail.com>
2026-08-26 13:10:50 +0200

    Refactor HumanPlayer to use try-with-resources for Scanner

--- src/main/java/ch/bbw/m450/tictactoe/players/HumanPlayer.java
+++ src/main/java/ch/bbw/m450/tictactoe/players/HumanPlayer.java
@@ -14,7 +14,8 @@ public class HumanPlayer implements TicTacToePlayer {
 	@Override
 	public int play(Stone[] board, Stone colorToPlay) {
 		System.out.println(TicTacToeMain.toString(board) + "where to to put the next " + colorToPlay + "? (0-8): ");
-		var scanner = new Scanner(System.in, StandardCharsets.UTF_8);
-		return Integer.parseInt(scanner.nextLine());
+		try (var scanner = new Scanner(System.in, StandardCharsets.UTF_8)) {
+			return Integer.parseInt(scanner.nextLine());
+		}
 	}
 }
